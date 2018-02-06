4192468f9022f7b23a0c01a8e090f23dbc7b19b6
Francisco <Francisco@192.168.1.107>
2018-02-05 22:42:59 -0500

    packagesu

---
+++ ProyectoHiperCorp/src/proyectohipercorp/vistas/FrmListaArticulo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectohipercorp.vistas;
+
+/**
+ *
+ * @author Francisco
+ */
+public class FrmListaArticulo {
+    
+}
