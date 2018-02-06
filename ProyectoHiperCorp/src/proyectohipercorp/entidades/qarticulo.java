209a4474309d0264dc0edbb5310540d4aa6fb82e
DiegoPatricio <DiegoPatricio@VAIO>
2018-02-05 19:52:21 -0500

    Rename

---
+++ ProyectoHiperCorp/src/proyectohipercorp/entidades/qarticulo.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectohipercorp.entidades;
+
+/**
+ *
+ * @author Usuario
+ */
+public class qarticulo {
+    private int idProveedor;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private int ruc;
+
+    public qarticulo() {
+    }
+
+    public qarticulo(int idProveedor, String nombre, String apellido, String direccion, int ruc) {
+        this.idProveedor = idProveedor;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.ruc = ruc;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(int ruc) {
+        this.ruc = ruc;
+    }
+    
+    
+    
+}
---
+++ ProyectoHiperCorp/src/proyectohipercorp/entidades/qproveedor.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectohipercorp.entidades;
+
+/**
+ *
+ * @author Usuario
+ */
+public class qproveedor {
+    private int idProveedor;
+    private int ruc; 
+      private String nombre;
+      private String apellido;
+      private String direccion;
+
+    public qproveedor() {
+    }
+
+    public qproveedor(int idProveedor, int ruc, String nombre, String apellido, String direccion) {
+        this.idProveedor = idProveedor;
+        this.ruc = ruc;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public int getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(int ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+      
+      
+      
+}
