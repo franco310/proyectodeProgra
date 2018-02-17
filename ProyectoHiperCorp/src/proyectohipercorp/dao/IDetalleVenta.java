733a96811152547b1bfe20f6b1a54a565e3a8239
Usuario <Usuario@DESKTOP-17ILUSK>
2018-02-17 15:25:31 -0500

    MarcoAmancha

--- ProyectoHiperCorp/src/proyectohipercorp/dao/IDetalleVenta.java
+++ ProyectoHiperCorp/src/proyectohipercorp/dao/IDetalleVenta.java
@@ -3,9 +3,9 @@ import proyectohipercorp.entidades.*;
 import java.util.*;
 
 public interface IDetalleVenta {
-    public int insertar(Detalle_venta detalleventa) throws Exception;
-    public int modificar(Detalle_venta detalleventa) throws Exception;
-    public int eliminar(Detalle_venta detalleventa) throws Exception;
-    public Detalle_venta obtener(int codigoDetalleVenta) throws Exception;
-    public List<Detalle_venta> obtener() throws Exception;    
+    public int insertar(DetalleVenta detalleventa) throws Exception;
+    public int modificar(DetalleVenta detalleventa) throws Exception;
+    public int eliminar(DetalleVenta detalleventa) throws Exception;
+    public DetalleVenta obtener(int codigoDetalleVenta) throws Exception;
+    public List<DetalleVenta> obtener() throws Exception;    
 }
