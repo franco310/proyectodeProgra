a91ce346791203ed7ae8cafb3c46f2f5f054f389
Segovia <Segovia@DESKTOP-2GC2MO3>
2018-02-17 15:31:58 -0500

    impl

---
+++ ProyectoHiperCorp/src/proyectohipercorp/entidades/Detalle_venta.java
@@ -0,0 +1,65 @@
+
+package proyectohipercorp.entidades;
+
+import java.sql.Date;
+
+public class Detalle_venta {
+    private int idCodigodetalle_venta;
+    private  Producto producto;
+    private FacturaVenta facturaventa;
+    private int precio;
+    private int preciototal;
+
+    public Detalle_venta() {
+    }
+
+    public Detalle_venta(int idCodigodetalle_venta, Producto producto, FacturaVenta facturaventa, int precio, int preciototal) {
+        this.idCodigodetalle_venta = idCodigodetalle_venta;
+        this.producto = producto;
+        this.facturaventa = facturaventa;
+        this.precio = precio;
+        this.preciototal = preciototal;
+    }
+
+    public int getIdCodigodetalle_venta() {
+        return idCodigodetalle_venta;
+    }
+
+    public void setIdCodigodetalle_venta(int idCodigodetalle_venta) {
+        this.idCodigodetalle_venta = idCodigodetalle_venta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public FacturaVenta getFacturaventa() {
+        return facturaventa;
+    }
+
+    public void setFacturaventa(FacturaVenta facturaventa) {
+        this.facturaventa = facturaventa;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getPreciototal() {
+        return preciototal;
+    }
+
+    public void setPreciototal(int preciototal) {
+        this.preciototal = preciototal;
+    }
+    
+    
+}
